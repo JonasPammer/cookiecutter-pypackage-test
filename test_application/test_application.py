08bcce175e8a46a172388f52417232093ebf5051
Jonas Pammer <opensource@jonaspammer.at>
2022-07-29 22:15:06 +0200

    chore: cruft update -y

--- test_application/test_application.py
+++ test_application/test_application.py
@@ -4,9 +4,12 @@ import click
 
 
 @click.command()
-def main() -> int:
-    return 0
+def main() -> None:
+    # NOTE: click does not pass on the return value of the function.
+    # use `raise SystemExit(return_code)` yourself where necessary
+    pass
 
 
 if __name__ == "__main__":
-    raise SystemExit(main())
+
+    main()
